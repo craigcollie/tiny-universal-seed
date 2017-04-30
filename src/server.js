af73d68094657752d9ca3b37c6ba512b9e80de84
Craig Collie <craig.collie@rea-group.com>
2017-04-30 14:51:19 +1000

    Add template tokenization

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import { createTinyServer } from 'tiny-universal';
 
 import App from './App';
 import Routes from './Routes';
-const indexTemplate = './../public/index.ejs';
+
+const templateString = fs.readFileSync('./index.html');
 
 const app = express();
-const tinyServer = createTinyServer(App, Routes, indexTemplate);
 
-app.use(tinyServer);
+app.use(
+  createTinyServer({
+    clientApp: App,
+    routes: Routes,
+    template: templateString,
+  })
+);
 
 app.listen(8080);
