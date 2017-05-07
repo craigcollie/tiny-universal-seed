79f0f7cfc6167aa4d34caf82502eb0dc61d3f30f
Craig Collie <craig.collie@rea-group.com>
2017-05-07 16:59:53 +1000

    Use RouteProvider

--- src/App.js
+++ src/App.js
@@ -1,56 +1,14 @@
 import React from 'react';
-import { Router, RoutingProvider, Link } from 'tiny-universal';
+import { Router, RouteProvider } from 'tiny-universal';
 
+import Demo from './Demo';
 import Routes from './Routes';
 
 const Root = (serverProps) => (
-  <RoutingProvider {...serverProps} routes={Routes}>
-    <ul>
-      <li>
-        <h2>Index route</h2>
-        <Link to="/" text="Home" />
-      </li>
-
-      <li>
-        <h2>Route with params /page/:id</h2>
-        <p>Should create the route and pass <strong>{'/page/:id'}</strong> with the values
-        in the URL</p>
-        <ul>
-          <li>
-            <Link to="/page/1" text="Page 1" />
-          </li>
-          <li>
-            <Link to="/page/2" text="page 2" />
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <h2>Route with multiple params /page/:id</h2>
-        <p>Should create the route and pass <strong>{'/:segment/:path/:blah'}</strong> with the values
-        in the URL</p>
-        <ul>
-          <li>
-            <Link to="/foo/bar/wee" text="/foo/bar/wee" />
-          </li>
-          <li>
-            <Link to="/1/2/foo" text="/1/2/foo" />
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <h2>Route with query params</h2>
-        <Link to="/foo?page=1" text="/foo?page=1" />
-      </li>
-
-      <li>
-        <h2>No data resolving</h2>
-        <Link to="/no-route-resolve" text="No route resolve" />
-      </li>
-    </ul>
+  <RouteProvider {...serverProps} routes={Routes}>
+    <Demo />
     <Router />
-  </RoutingProvider>
+  </RouteProvider>
 );
 
 export default Root;
