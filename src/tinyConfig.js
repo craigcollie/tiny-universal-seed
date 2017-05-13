715114f87ee215530916df577f50e42615f44553
Craig Collie <craig.collie@rea-group.com>
2017-05-13 10:49:26 +1000

    Update config

--- src/tinyConfig.js
+++ src/tinyConfig.js
@@ -7,12 +7,4 @@ export default {
     routes: Routes,
     template: './index.html',
   },
-
-  //  Lifecycle methods
-  rootResolve: () => {
-    console.log('root resolve');
-  },
-  afterRouteChange: () => {
-    console.log('after route change');
-  },
 };
